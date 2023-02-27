3fd1f069bab766289a46210c4c7bcb98db1cb87b
Baltoch <balthazar.lebreton@gmail.com>
2023-02-27 12:52:05 +0000

    add User class

---
+++ CarRentingApp/src/carrentingapp/User.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package carrentingapp;
+
+/**
+ *
+ * @author balth
+ */
+public class User {
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private DriverLicense driverLicense;
+    
+    public void addToDB()
+    {
+        
+    }
+    
+    public void removeFromDB()
+    {
+        
+    }
+    
+    public void updateInDB()
+    {
+        
+    }
+    
+    public Rental bookRental()
+    {
+        
+    }
+    
+    public void pay()
+    {
+        
+    }
+    
+    public void review()
+    {
+        
+    }
+}
