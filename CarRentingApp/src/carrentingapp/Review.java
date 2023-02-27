1dd7d6a6acdd5a04570737b0b90a89b6cf91e3c0
Baltoch <balthazar.lebreton@gmail.com>
2023-02-27 13:06:09 +0000

    add Review class

---
+++ CarRentingApp/src/carrentingapp/Review.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package carrentingapp;
+
+/**
+ *
+ * @author balth
+ */
+public class Review {
+    private int reviewId;
+    private String message;
+    private Rental rental;
+    private User user;
+    
+        
+    public void addToDB()
+    {
+        
+    }
+    
+    public void removeFromDB()
+    {
+        
+    }
+    
+    public void updateInDB()
+    {
+        
+    }
+}
