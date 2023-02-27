7f4ff4514a0316ac291b24c69ff16450d59acfac
Baltoch <balthazar.lebreton@gmail.com>
2023-02-27 12:45:21 +0000

    add Transaction class

---
+++ CarRentingApp/src/carrentingapp/Transaction.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package carrentingapp;
+
+/**
+ *
+ * @author balth
+ */
+public class Transaction {
+    private String transactionId;
+    private String status;
+    private double amount;
+    private double paidAmount;
+    private User debtor;
+    
+    public void addToDB()
+    {
+        
+    }
+    
+    public void removeFromDB()
+    {
+        
+    }
+    
+    public void updateInDB()
+    {
+        
+    }
+}
