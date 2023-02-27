a0bd45ff10468271076c2b7bc70fab03275852b9
Baltoch <balthazar.lebreton@gmail.com>
2023-02-27 12:35:30 +0000

    First import

---
+++ CarRentingApp/src/carrentingapp/Insurance.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package carrentingapp;
+
+/**
+ *
+ * @author balth
+ */
+public class Insurance {
+    private int contactNumber;
+    private String coverage;
+    private String conditions;
+    private double price;
+    
+    public void addToDB()
+    {
+        
+    }
+    
+    public void removeFromDB()
+    {
+        
+    }
+    
+    public void updateInDB()
+    {
+        
+    }
+}
