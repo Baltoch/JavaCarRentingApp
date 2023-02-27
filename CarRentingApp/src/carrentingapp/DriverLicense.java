6f4b49704b6ef0f723839817f5caad409c6ec4d7
Baltoch <balthazar.lebreton@gmail.com>
2023-02-27 12:54:06 +0000

    add DriverLicense class

---
+++ CarRentingApp/src/carrentingapp/DriverLicense.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package carrentingapp;
+
+/**
+ *
+ * @author balth
+ */
+public class DriverLicense {
+    private String driverLicenseNumber;
+        
+    public void addToDB()
+    {
+        
+    }
+    
+    public void removeFromDB()
+    {
+        
+    }
+    
+    public void updateInDB()
+    {
+        
+    }
+}
