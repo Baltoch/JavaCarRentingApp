d1555b810aed987ba8cb6bf4eff4e35e06f98eda
Baltoch <balthazar.lebreton@gmail.com>
2023-03-01 22:38:39 +0000

    Add Photo Class

---
+++ CarRentingApp/src/carrentingapp/Photo.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package carrentingapp;
+
+/**
+ *
+ * @author balth
+ */
+public class Photo {
+    private String accessLink;
+    private Car car;
+            
+    public void addToDB()
+    {
+        
+    }
+    
+    public void removeFromDB()
+    {
+        
+    }
+    
+    public void updateInDB()
+    {
+        
+    }
+}
