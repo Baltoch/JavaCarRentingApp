d9af9851ba6f5a6446490e46d64183592f8f1082
Baltoch <balthazar.lebreton@gmail.com>
2023-03-01 22:32:45 +0000

    Add Car Class

---
+++ CarRentingApp/src/carrentingapp/Car.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package carrentingapp;
+
+/**
+ *
+ * @author balth
+ */
+public class Car {
+    private String registrationNumber;
+    private int mileage;
+    private Location location;
+    private Model model;
+            
+    public void addToDB()
+    {
+        
+    }
+    
+    public void removeFromDB()
+    {
+        
+    }
+    
+    public void updateInDB()
+    {
+        
+    }
+}
