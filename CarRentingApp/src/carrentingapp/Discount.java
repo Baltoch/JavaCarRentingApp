4bc5a34fda825f550a8703da5f0750178cb65f6b
Baltoch <balthazar.lebreton@gmail.com>
2023-03-01 12:37:33 +0000

    Add Discount Class

---
+++ CarRentingApp/src/carrentingapp/Discount.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package carrentingapp;
+import java.sql.Date;
+/**
+ *
+ * @author balth
+ */
+public class Discount {
+    private double discountLevel;
+    private Date startingDate;
+    private Date endingDate;
+    private Employee employee;
+    private Car car;
+            
+    public void addToDB()
+    {
+        
+    }
+    
+    public void removeFromDB()
+    {
+        
+    }
+    
+    public void updateInDB()
+    {
+        
+    }
+}
