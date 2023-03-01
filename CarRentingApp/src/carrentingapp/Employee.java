086e1009ee8ecaf0fb55bd2f28e65af27e80bc54
Baltoch <balthazar.lebreton@gmail.com>
2023-03-01 12:48:38 +0000

    Add Employee Class

---
+++ CarRentingApp/src/carrentingapp/Employee.java
@@ -0,0 +1,129 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package carrentingapp;
+
+/**
+ *
+ * @author balth
+ */
+public class Employee extends User {
+    private double baseSalary;
+    private String jobTitle;
+            
+    public void addToDB()
+    {
+        
+    }
+    
+    public void removeFromDB()
+    {
+        
+    }
+    
+    public void updateInDB()
+    {
+        
+    }
+    
+    public void moveCar()
+    {
+        
+    }
+    
+    public void addCar()
+    {
+        
+    }
+    
+    public void removeCar()
+    {
+        
+    }
+    
+    public void reviewRental()
+    {
+        
+    }
+    
+    public void addLocation()
+    {
+        
+    }
+    
+    public void removeLocation()
+    {
+        
+    }
+    
+    public double computeCurrentBonus()
+    {
+        
+    }
+    
+    public double computeEstimatedSalary()
+    {
+        
+    }
+    
+    public void addDiscount()
+    {
+        
+    }
+    
+    public void removeDiscount()
+    {
+        
+    }
+    
+    public void updateDiscount()
+    {
+        
+    }
+    
+    public void addMember()
+    {
+        
+    }
+    
+    public void updateMember()
+    {
+        
+    }
+    
+    public void removeMember()
+    {
+        
+    }
+    
+    public void updateRental()
+    {
+        
+    }
+    
+    public void removeRental()
+    {
+        
+    }
+    
+    public void addRental()
+    {
+        
+    }
+    
+    public void addInsurance()
+    {
+        
+    }
+    
+    public void updateInsurance()
+    {
+        
+    }
+    
+    public void removeInsurance()
+    {
+        
+    }
+}
