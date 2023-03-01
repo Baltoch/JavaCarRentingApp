a8848ee5f1e225e63a9a2b1401729fbe0fe574e5
Baltoch <balthazar.lebreton@gmail.com>
2023-03-01 22:26:55 +0000

    Add Location Class

---
+++ CarRentingApp/src/carrentingapp/Location.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package carrentingapp;
+
+/**
+ *
+ * @author balth
+ */
+public class Location {
+    private int locationId;
+    private String name;
+            
+    public void addToDB()
+    {
+        
+    }
+    
+    public void removeFromDB()
+    {
+        
+    }
+    
+    public void updateInDB()
+    {
+        
+    }
+}
