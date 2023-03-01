fcd960b6ab4f25e6b473b1c687cfea6ae5691f2b
Baltoch <balthazar.lebreton@gmail.com>
2023-03-01 22:36:55 +0000

    Add Model Class

---
+++ CarRentingApp/src/carrentingapp/Model.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package carrentingapp;
+
+/**
+ *
+ * @author balth
+ */
+public class Model {
+    private String model;
+    private String brand;
+    private double dailyRate;
+    private int numOfSeats;
+    private int bootVolume;
+    private double additionalMilePrice;
+    private char transmissionType;
+    private int horsePower;
+    private String type;
+            
+    public void addToDB()
+    {
+        
+    }
+    
+    public void removeFromDB()
+    {
+        
+    }
+    
+    public void updateInDB()
+    {
+        
+    }
+}
