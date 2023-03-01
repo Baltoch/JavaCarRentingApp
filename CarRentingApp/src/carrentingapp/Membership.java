9445fade69fd9a56fa1b25454faf627511302db1
Baltoch <balthazar.lebreton@gmail.com>
2023-03-01 12:34:47 +0000

    Add Membership Class

---
+++ CarRentingApp/src/carrentingapp/Membership.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package carrentingapp;
+
+/**
+ *
+ * @author balth
+ */
+public class Membership {
+    private String memebershipType;
+    private double price;
+    private double additionalDiscount;
+            
+    public void addToDB()
+    {
+        
+    }
+    
+    public void removeFromDB()
+    {
+        
+    }
+    
+    public void updateInDB()
+    {
+        
+    }
+}
